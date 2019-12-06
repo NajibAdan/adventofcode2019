918f3bb4deb075d000de741cf346bd12caf883d2
Najib Adan <najibadan@airmail.cc>
2019-12-06 18:19:26 +0300

    Day 01 complete

--- day01/fuel_consumption.py
+++ day01/fuel_consumption.py
@@ -1,9 +1,15 @@
 import math
 def getConsumption(mass):
     return math.floor(int(mass)/3)-2
+
+def recursiveConsumption(mass):
+    if math.floor(mass/3) >= 0:
+        return mass + recursiveConsumption(getConsumption(mass))
+    else:
+        return 0
 modules = open("./day01/input.txt",'r')
 sum = 0
 for module in modules:
-    sum = sum + getConsumption(module)
+    sum = sum + recursiveConsumption(getConsumption(module))
 
 print(sum)
