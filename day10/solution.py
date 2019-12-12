d84d90e4454330159ccda039272364c04c7a9cd3
Najib Adan <najibadan@airmail.cc>
2019-12-12 08:45:56 +0300

    Day 10 completed

--- day10/solution.py
+++ day10/solution.py
@@ -6,8 +6,35 @@ def deg(point,data):
         if point != p:
             new = (point[0]-p[0],point[1]-p[1])
             degrees.append(math.atan2(new[1],new[0]))
-    return len(set(degrees))
+    return len(set(degrees)), degrees
     
+def nuker(data,point):
+	count = 0
+	final_angle = -1e-100
+	while count < 200:
+		count +=1
+		nukedx,nukedy = 0,0
+		nuked_dist = 1e99
+		angle = 10
+		for x,y in data:
+			new_angle = math.atan2(point[0]-x,point[1]-y)
+			if new_angle < 0:
+				new_angle += (2 *math.pi)
+			if new_angle != 0:
+				new_angle = 2 * math.pi - new_angle
+			if new_angle > final_angle:
+				distance = (x-point[0])**2 + (y-point[1])**2
+				if new_angle < angle or (new_angle == angle and distance < nuked_dist):
+					nukedx,nukedy = x,y
+					nuked_dist,angle = distance,new_angle
+		if angle == 10:
+			final_angle = -1e-100
+			count -=1
+			continue
+		data.remove((nukedx,nukedy))
+		final_angle = angle
+	return nukedx*100+nukedy
+
 co = []
 file = open('input.txt')
 x,y = 0,0
@@ -21,8 +48,14 @@ for line in file:
 
 count = {}
 for point in co:
-    count[point] = deg(point,co)
+    number,degrees = deg(point,co)
+    count[point] = number
+pl = list(count.keys())[list(count.values()).index(max(count.values()))]
+# Part One
+print(pl,max(count.values()))
 
-print(list(count.keys())[list(count.values()).index(max(count.values()))],max(count.values())) 
+# Part Two
+co.remove(pl)
 
+print(nuker(set(co),pl))
 
